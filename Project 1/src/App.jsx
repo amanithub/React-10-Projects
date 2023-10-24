97f465f4bedda4bcbb1e7412576850e9d301ed83
amanithub <pandeyamankumar13@gmail.com>
2023-10-24 20:07:02 +0530

    final commit

---
+++ Project 1/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import HeroSection from "./components/Hero.jsx";
+import Navigation from "./components/Navigation.jsx";
+
+
+const App = () =>{
+
+return (
+
+<div>
+ <Navigation />
+ <HeroSection />
+</div>
+);
+
+
+};
+
+export default  App;
